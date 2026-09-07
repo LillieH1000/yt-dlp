c47780e0c794ec250d102ba139dab8f7ab2dcdc2
Lillie <91358136+LillieH1000@users.noreply.github.com>
2026-09-07 17:35:56 -0400

    Hatch fmttttttttttttttttt

--- yt_dlp/extractor/pixabay.py
+++ yt_dlp/extractor/pixabay.py
@@ -24,46 +24,47 @@ class PixabayBaseIE(InfoExtractor):
             'title': ('title', {str}),
         })
 
+
 class PixabaySoundMusicIE(PixabayBaseIE):
     _VALID_URL = r'https?://(?:www\.)?pixabay\.com/(?:music|sound-effects)/(?:[^/?#]+-)?(?P<id>\d+)'
     _TESTS = [
-    # Music
-    {
-        'url': 'https://pixabay.com/music/future-bass-no-copyright-music-537751/',
-        'info_dict': {
-            'id': '537751',
-            'ext': 'mp3',
-            'thumbnail': r're:^https?://.*\(?:png|jpg)',
-            'description': 'md5:45435d2ca9aba2167f98c8ed61ec105a',
-            'title': 'No Copyright Music by SigmaMusicArt| Royalty-free Music',
+        # Music
+        {
+            'url': 'https://pixabay.com/music/future-bass-no-copyright-music-537751/',
+            'info_dict': {
+                'id': '537751',
+                'ext': 'mp3',
+                'thumbnail': r're:^https?://.*\(?:png|jpg)',
+                'description': 'md5:45435d2ca9aba2167f98c8ed61ec105a',
+                'title': 'No Copyright Music by SigmaMusicArt| Royalty-free Music',
+            },
         },
-    },
-    # Sound Effect
-    {
-        'url': 'https://pixabay.com/sound-effects/film-special-effects-calm-inspiring-technology-logo-short-version-518993/',
-        'info_dict': {
-            'id': '518993',
-            'ext': 'mp3',
-            'thumbnail': r're:^https?://.*\(?:png|jpg)',
-            'description': 'md5:9d3ae9314a08fa7c1ebfef797abd5068',
-            'title': 'Calm Inspiring Technology Logo (Short Version) by AleXZavesa| Royalty-free Music',
+        # Sound Effect
+        {
+            'url': 'https://pixabay.com/sound-effects/film-special-effects-calm-inspiring-technology-logo-short-version-518993/',
+            'info_dict': {
+                'id': '518993',
+                'ext': 'mp3',
+                'thumbnail': r're:^https?://.*\(?:png|jpg)',
+                'description': 'md5:9d3ae9314a08fa7c1ebfef797abd5068',
+                'title': 'Calm Inspiring Technology Logo (Short Version) by AleXZavesa| Royalty-free Music',
+            },
         },
-    },
-    # Sound Effect - no thumbnail
-    {
-        'url': 'https://pixabay.com/sound-effects/musical-relaxing-guitar-loop-v5-245859/',
-        'info_dict': {
-            'id': '537751',
-            'ext': 'mp3',
-            'description': 'md5:64136651e70263d7fc965fe8a0d61435',
-            'title': 'Relaxing Guitar Loop V5 by IdoBerg| Royalty-free Music',
-        },
-    }]
+        # Sound Effect - no thumbnail
+        {
+            'url': 'https://pixabay.com/sound-effects/musical-relaxing-guitar-loop-v5-245859/',
+            'info_dict': {
+                'id': '537751',
+                'ext': 'mp3',
+                'description': 'md5:64136651e70263d7fc965fe8a0d61435',
+                'title': 'Relaxing Guitar Loop V5 by IdoBerg| Royalty-free Music',
+            },
+        }]
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
         info = self._get_audio_info(url, video_id)
-        
+
         return {
             'id': video_id,
             'vcodec': 'none',
