1faaeae2f9898950bf14dffd66f36b865074ea2e
Lillie <91358136+LillieH1000@users.noreply.github.com>
2026-09-07 17:21:49 -0400

    Add comment for headers

--- yt_dlp/extractor/pixabay.py
+++ yt_dlp/extractor/pixabay.py
@@ -4,6 +4,7 @@ from ..utils.traversal import traverse_obj
 
 
 class PixabayBaseIE(InfoExtractor):
+    # Cookie header is required or the request will 403
     _headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/152.0.0.0 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
