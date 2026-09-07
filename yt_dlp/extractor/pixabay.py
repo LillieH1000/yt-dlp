afd3d1aed0e4920a9c52c98927381b21a30096a7
Lillie <91358136+LillieH1000@users.noreply.github.com>
2026-09-07 17:21:42 -0400

    Add required headers for webpage request

--- yt_dlp/extractor/pixabay.py
+++ yt_dlp/extractor/pixabay.py
@@ -4,6 +4,14 @@ from ..utils.traversal import traverse_obj
 
 
 class PixabayBaseIE(InfoExtractor):
+    _headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/152.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Referer': 'https://pixabay.com/',
+        'Cookie': 'is_human=1;',
+    }
+
     def _get_page_props(self, url, video_id):
         webpage = self._download_webpage(url, video_id)
         return self._search_nextjs_data(webpage, video_id)['props']['pageProps']
